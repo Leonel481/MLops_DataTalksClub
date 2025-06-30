4356afa4ac1b04bc27dcd8ca9eda1d9ddacc3bf0
Leonel481 <leonel.aliaga.v@gmail.com>
2025-06-30 17:58:48 -0500

    testing

--- 06. Best practice/model.py
+++ 06. Best practice/model.py
@@ -29,22 +29,24 @@ def base64_decode(encoded_data):
 
 class ModelService:
 
-    def prepare_feature(ride):
+    def __init__(self, model, model_version=None, callbacks=None):
+        self.model = model
+        self.model_version = model_version
+        self.callbacks = callbacks or []
+
+    def prepare_feature(self,ride):
         feature = {}
         feature['PU_DO'] = f"{ride['PULocationID']}_{ride['DOLocationID']}"
         feature['trip_distance'] = ride['trip_distance']
 
         return feature
 
-
-    def predict(features):
-        preds = model.predict(features)
+    def predict(self, features):
+        preds = self.model.predict(features)
         return float(preds[0])
 
 
-    def lambda_handler(event, context):
-
-        # print(json.dumps(event))
+    def lambda_handler(self, event):
 
         predictions = []
 
@@ -57,8 +59,8 @@ class ModelService:
             ride = ride_event['ride']
             ride_id = ride_event['ride_id']
 
-            features = prepare_feature(ride)
-            prediction = predict(features)
+            features = self.prepare_feature(ride)
+            prediction = self.predict(features)
 
             predictions_event = {
                 'model': 'ride_duration_prediuction_model',
@@ -69,23 +71,50 @@ class ModelService:
                 }
             }
 
-            if not TEST_RUN:
-                kinesis_client.put_record(
-                    StreamName = PREDICTION_STREAM_NAME,
-                    Data = json.dumps(predictions_event),
-                    PartitionKey = str(ride_id)
-                )
+            for callback in self.callbacks:
+                callback(predictions_event)
 
             predictions.append(predictions_event)
 
         return {
             'predictions' : predictions
         }
+    
 
-    kinesis_client = boto3.client('kinesis')
-    class ModelService():
+class KinesisCallback:
+
+    def __init__(self, kinesis_client, prediction_stream_name):
+        self.kinesis_client = kinesis_client
+        self.prediction_stream_name = prediction_stream_name
+
+    def put_record(self, prediction_event):
+        ride_id = prediction_event['prediction']['ride_id']
+
+        self.kinesis_client.put_record(
+            StreamName=self.prediction_stream_name,
+            Data=json.dumps(prediction_event),
+            PartitionKey=ride_id,
+        )
+
+def create_kinesis_client():
+    endpoint_url = os.getenv('KINESIS_ENDPOINT_URL', None)
+
+    if endpoint_url is None:
+        return boto3.client('kinesis')
+    
+    return boto3.client('kinesis', endpoint_url=endpoint_url)
 
 
-    def init(prediction_stream_name: str, run_id: str, test_run: bool):
+def init(prediction_stream_name: str, run_id: str, test_run: bool):
+    model = load_model(run_id)
 
-        return 
+    callbacks = []
+
+    if not test_run:
+        kinesis_client = create_kinesis_client()
+        kinesis_callback = KinesisCallback(kinesis_client, prediction_stream_name)
+        callbacks.append(kinesis_callback.put_record)
+
+    model_service = ModelService(model=model,model_version=run_id , callbacks=callbacks)
+
+    return model_service
--- 06. Best practice/tests/model_test.py
+++ 06. Best practice/tests/model_test.py
@@ -1,18 +1,88 @@
-# import lambda_function
+import model
 
-def test_prepare_feature():
-    # ride = {
-    #     'PULocationID': 130,
-    #     'DOLocationID': 205,
-    #     'trip_distance': 3.66
-    # }
 
-    # actual_feature = lambda_function.prepare_feature(ride)
-    # expected_feature = {
-    #     'PU_DO': '130_205',
-    #     'trip_distance': 3.66
-    # }
+def test_base64_decode():
+    base64_input = "ewogICAgICAgICJyaWRlIjogewogICAgICAgICAgICAiUFVMb2NhdGlvbklEIjogMTMwLAogICAgICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICAgICAidHJpcF9kaXN0YW5jZSI6IDMuNjYKICAgICAgICB9LCAKICAgICAgICAicmlkZV9pZCI6IDI1NgogICAgfQ=="
+    actual_result = model.base64_decode(base64_input)
+    expected_result = {
+        'ride': {
+            'PULocationID': 130,
+            'DOLocationID': 205,
+            'trip_distance': 3.66
+        },
+        'ride_id': 256
+    }
 
-    # assert actual_feature == expected_feature
+    assert actual_result == expected_result
 
-    assert 1==1
+
+def test_prepare_features():
+    model_service = model.ModelService(None)
+
+    ride = {
+        "PULocationID": 130,
+        "DOLocationID": 205,
+        "trip_distance": 3.66,
+    }
+
+    actual_features = model_service.prepare_feature(ride)
+
+    expected_fetures = {
+        "PU_DO": "130_205",
+        "trip_distance": 3.66,
+    }
+
+    assert actual_features == expected_fetures
+
+class ModelMock:
+    def __init__(self,value):
+        self.value = value
+
+    def predict(self, x):
+        n = len(x)
+        return [self.value] * n
+
+
+def test_predict():
+    model_mock = ModelMock(10.0)
+    model_service = model.ModelService(model_mock)
+
+    features = {
+        "PU_DO":"130_205",
+        "trip_distance":3.66
+    }
+
+    actual_prediction = model_service.predict(features)
+    expected_prediction = 10.0
+
+    assert actual_prediction == expected_prediction
+
+def test_lambda_handler():
+
+    model_mock = ModelMock(10.0)
+    model_version = "123"
+    model_service = model.ModelService(model_mock, model_version)
+
+    event = {
+        "Records": [
+            {
+                "kinesis": {
+                    "data": "ewogICAgICAgICJyaWRlIjogewogICAgICAgICAgICAiUFVMb2NhdGlvbklEIjogMTMwLAogICAgICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICAgICAidHJpcF9kaXN0YW5jZSI6IDMuNjYKICAgICAgICB9LCAKICAgICAgICAicmlkZV9pZCI6IDI1NgogICAgfQ=="
+                }
+            }
+        ]
+    }
+
+    actual_prediction = model_service.lambda_handler(event)
+    expected_prediction = {
+        'predictions': [{
+            'model': 'ride_duration_prediuction_model',
+            'version' : model_version,
+            'prediction' : {
+                'ride_duration': 10.0,
+                'ride_id': 256
+            }
+        }]
+    }
+    
+    assert actual_prediction == expected_prediction
